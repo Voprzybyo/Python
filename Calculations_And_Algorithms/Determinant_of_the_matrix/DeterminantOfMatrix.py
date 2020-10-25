bcbdbe57aabf58f63cddaf3563c8d97e900e3356
Wojtek <wo.przybyo@gmail.com>
2020-10-25 18:55:33 +0100

    Determinant of matrix v2 added

--- Calculations_And_Algorithms/Determinant_of_the_matrix/DeterminantOfMatrix.py
+++ Calculations_And_Algorithms/Determinant_of_the_matrix/DeterminantOfMatrix.py
@@ -1,6 +1,18 @@
 #! /usr/bin/env python
 
 import random
+import numpy as np
+import math
+
+
+# Checking if algorithm works correctly
+def check_correctness(detA, detB):
+    detB=math.floor(detB+0.49999999)
+
+    if detA == detB:
+        print("Algorithm is working fine. Determinant of given matrix is: ", detB)
+    else:
+        print("Determinant of given matrix is: ", detB)
 
 
 # Function generating matrix with random content
@@ -14,35 +26,39 @@ def generate_random_matrix(rows, cols):
 # Calculate determinant of given matrix using recursion
 def calculate_determinant(X, n):
 
-    if len(X[0]) != len(X):   # Checking if number of column X is equal to number of rows Y (necessary condition)
-        print("Cannot calculate determinant due to wrong matrix dimension")
-
     det = 0
     matrix_temp = [[0 for i in range(n)] for j in range(n)]
+
     if n == 2:                                              # If matrix is 2x2...
-        return (X[0][0] * X[1][1]) - (X[1][0] * X[0][1])
+        det = (X[0][0] * X[1][1]) - (X[1][0] * X[0][1])
+        return det
     else:
-        for x in range(n):
-            temp_i = 0
-            for i in range(n):
-                temp_j = 0
+        for c in range(n):
+            subi = 0
+            for i in range(1, n):
+                subj = 0
                 for j in range(n):
-                    if j == x:
+                    if j == c:
                         continue
-                    matrix_temp[temp_i][temp_j] = X[i][j]
-                    temp_j += 1
-                temp_i += 1
-            det = det + ((-1 ** x) * X[0][x] * calculate_determinant(matrix_temp, n - 1))
+                    matrix_temp[subi][subj] = X[i][j]
+                    subj += 1
+                subi += 1
+            det = det + ((-1 ** c) * X[0][c] * calculate_determinant(matrix_temp, n - 1))
     return det
 
 
-rows, cols = (8, 8)
-
+# rows, cols = (8, 8)
+rows, cols = (3, 3)
 matrix = generate_random_matrix(rows, cols)
 
 for row in matrix:
     print(row)
 print("\n")
 
+# Calculate determinant (algorithm)
 res_matrix = calculate_determinant(matrix, rows)
-print(res_matrix)
+
+# Calculate determinant (numpy function)
+res_matrix_check = np.linalg.det(matrix)
+
+check_correctness(res_matrix, res_matrix_check)
